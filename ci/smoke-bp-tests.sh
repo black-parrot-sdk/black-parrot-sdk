d5b7f4d420ac7b5364d3c12e66773e702d3fcdb2
Dan Ruelas-Petrisko <petrisko.bsg@gmail.com>
2026-04-21 15:51:25 -0700

    bump bp-tests / add spike and dromajo patches

--- ci/smoke-bp-tests.sh
+++ ci/smoke-bp-tests.sh
@@ -4,7 +4,7 @@ source $(dirname $0)/functions.sh
 sim=$1; shift
 
 suite=bp-tests
-progs=(amo_nonblocking cache_flush cache_hammer constructor divide_hazard dram_stress fflags_haz fp_neg_zero_nanbox fp_precision hello_world loop mapping map mstatus_fs nanboxing paging satp_nofence stream_hammer template timer_interrupt interrupt_priority interrupt_priority_simultaneous interrupt_priority_race unwinding vector virtual)
+progs=(amo_nonblocking cache_flush cache_hammer constructor divide_hazard dram_stress fflags_haz fp_neg_zero_nanbox fp_precision hello_world loop mapping map mstatus_fs nanboxing paging satp_nofence stream_hammer template timer_interrupt interrupt_priority interrupt_priority_simultaneous interrupt_priority_race unwinding vector virtual out_of_bounds_test high_dram_alias)
 
 for prog in "${progs[@]}"; do
     do_single_sim ${sim} ${suite} ${prog} 1
